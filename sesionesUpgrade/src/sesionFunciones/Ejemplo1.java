c566378148aef100c646ad181dd2f9ea74f95a14
Franck <francklopez92@outlook.es>
2025-03-26 21:17:53 +0100

    update sesionFunciones

---
+++ sesionesUpgrade/src/sesionFunciones/Ejemplo1.java
@@ -0,0 +1,19 @@
+package sesionFunciones;
+
+public class Ejemplo1 {
+
+    public static void main(String[] args) {
+        // Voy a crear una funcion que me permite elevar un numero 'a' a una potencia
+        // 'b'
+        // La idea es que mas adelante esta siguiente linea funcione:
+
+        double resultado = potencia(2.8, 3.3);
+        System.out.println("El resultado de elevar 2.8 a la 3.3 es: " + resultado);
+    }
+
+    public static double potencia(double a, double b) {
+        double resultado = Math.pow(a, b);
+        return resultado;
+    }
+
+}
