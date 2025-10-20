d3c4c6e51db2738201aef9749394cc5ffed64335
Franck <francklopez92@outlook.es>
2025-10-20 22:05:03 +0200

    Practice myself

---
+++ untitled/src/Main.java
@@ -0,0 +1,23 @@
+import java.time.Clock;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 4;
+
+        // Realiza las siguientes operaciones:
+        // 1. Suma de a + b
+        System.out.println("1. Suma de a + b :" + (a + b));
+        // 2. Resta de a - b
+        System.out.println("2. Resta de a - b : " + (a - b));
+        // 3. Multiplicación de a * b
+        System.out.println("3. Multiplicación de a * b :" + (a * b));
+        // 4. División de a / b
+        System.out.println("4. División de a / b : " + (a / b));
+        // 5. Módulo de a % b
+        System.out.println("5. Módulo de a % b : " + (a % b));
+    }
+
+}
